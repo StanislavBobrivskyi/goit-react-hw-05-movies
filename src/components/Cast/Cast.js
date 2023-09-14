627ebd7f036070a68d31208d461d0e95b9282af7
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-09-14 10:21:11 +0200

    changes

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,32 +1,32 @@
-// Cast.js
+// // Cast.js
 
-import React, { useEffect, useState } from 'react';
-import { getMovieCredits } from '../API'; // Імпортуємо функцію з API.js
+// import React, { useEffect, useState } from 'react';
+// import { getMovieCredits } from '../API'; // Імпортуємо функцію з API.js
 
-function Cast({ movieId }) {
-  const [cast, setCast] = useState([]);
+// function Cast({ movieId }) {
+//   const [cast, setCast] = useState([]);
 
-  useEffect(() => {
-    // Викликаємо функцію для отримання акторського складу при завантаженні компонента
-    getMovieCredits(movieId)
-      .then(data => {
-        setCast(data);
-      })
-      .catch(error => {
-        console.error('Error fetching movie credits:', error);
-      });
-  }, [movieId]);
+//   useEffect(() => {
+//     // Викликаємо функцію для отримання акторського складу при завантаженні компонента
+//     getMovieCredits(movieId)
+//       .then(data => {
+//         setCast(data);
+//       })
+//       .catch(error => {
+//         console.error('Error fetching movie credits:', error);
+//       });
+//   }, [movieId]);
 
-  return (
-    <div>
-      <h2>Акторський склад:</h2>
-      <ul>
-        {cast.map(actor => (
-          <li key={actor.id}>{actor.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Акторський склад:</h2>
+//       <ul>
+//         {cast.map(actor => (
+//           <li key={actor.id}>{actor.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default Cast;
+// export default Cast;
--- src/components/api.js
+++ src/components/api.js
@@ -1,33 +1,70 @@
+// import axios from 'axios';
+
+// const apiKey =
+//   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTk0YjdiMDhmNDMzMTU3MDMyYTdhNWEzMTBjOTQ2ZiIsInN1YiI6IjY0ZmRlOGI2ZGI0ZWQ2MTAzODU0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sztJc-TGyzGJ71FcSXCwTZF-8--39MZuPwv_9dKwxLA';
+// const baseUrl = 'https://api.themoviedb.org/3';
+
+// const api = axios.create({
+//   baseURL: baseUrl,
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: `Bearer ${apiKey}`,
+//   },
+// });
+
+// export const getTrendingMovies = async () => {
+//   try {
+//     const response = await api.get('/trending/all/day?language=en-US');
+//     return response.data.results; // Повертаємо лише масив фільмів з результатів
+//   } catch (error) {
+//     console.error('There was a problem with the Axios request:', error);
+//     throw error; // Передаємо помилку наверх для обробки її в компоненті
+//   }
+// };
+
+// export const getMovieCredits = async movieId => {
+//   try {
+//     const response = await api.get(`/movie/${movieId}/credits`);
+//     return response.data.cast; // Повертаємо лише акторський склад з результатів
+//   } catch (error) {
+//     console.error('There was a problem with the Axios request:', error);
+//     throw error; // Передаємо помилку наверх для обробки її в компоненті
+//   }
+// };
+
 import axios from 'axios';
 
-const apiKey =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTk0YjdiMDhmNDMzMTU3MDMyYTdhNWEzMTBjOTQ2ZiIsInN1YiI6IjY0ZmRlOGI2ZGI0ZWQ2MTAzODU0Y2VhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sztJc-TGyzGJ71FcSXCwTZF-8--39MZuPwv_9dKwxLA';
-const baseUrl = 'https://api.themoviedb.org/3';
-
-const api = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${apiKey}`,
-  },
-});
-
-export const getTrendingMovies = async () => {
-  try {
-    const response = await api.get('/trending/all/day?language=en-US');
-    return response.data.results; // Повертаємо лише масив фільмів з результатів
-  } catch (error) {
-    console.error('There was a problem with the Axios request:', error);
-    throw error; // Передаємо помилку наверх для обробки її в компоненті
-  }
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+// const API_KEY = '7194b7b08f433157032a7a5a310c946f';
+axios.defaults.params = {
+  api_key: '7194b7b08f433157032a7a5a310c946f',
 };
 
-export const getMovieCredits = async movieId => {
-  try {
-    const response = await api.get(`/movie/${movieId}/credits`);
-    return response.data.cast; // Повертаємо лише акторський склад з результатів
-  } catch (error) {
-    console.error('There was a problem with the Axios request:', error);
-    throw error; // Передаємо помилку наверх для обробки її в компоненті
-  }
+export async function fetchTrendingMovies() {
+  const resp = await axios.get(`trending/movie/day`);
+
+  return resp.data;
+}
+
+export const fetchMoviesById = async moviesId => {
+  const resp = await axios.get(`/movie/${moviesId}?&language=en-US`);
+
+  return resp.data;
 };
+
+export async function fetchMoviesBySearch(searchQuery) {
+  const resp = await axios.get(`search/movie?query=${searchQuery}`);
+
+  return resp.data;
+}
+
+export async function fetchCastMovie(moviesId) {
+  const resp = await axios.get(`movie/${moviesId}/credits`);
+
+  return resp.data;
+}
+export async function fetchReviwsMovie(moviesId) {
+  const resp = await axios.get(`movie/${moviesId}/reviews`);
+
+  return resp.data;
+}
